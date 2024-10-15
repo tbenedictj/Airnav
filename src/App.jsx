658085001f6a0a56c1ae29ddc40ee72b74b8d07a
Tumewu Benedict Jonathan <tbenedictjonathan@gmail.com>
2024-10-16 04:06:07 +0800

    Add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import Sidebar from './Component/sideBar';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Sidebar />
-    </>
-  )
+    <main className='app'>
+      <Sidebar />
+    </main>
+  );
 }
-
-export default App

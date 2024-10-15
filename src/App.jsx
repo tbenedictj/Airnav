c53aef1e0ca85e8498b330729fa1c4b8314fb269
Tumewu Benedict Jonathan <tbenedictjonathan@gmail.com>
2024-10-16 02:05:14 +0800

    add lucide-react  library

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -11,6 +9,7 @@ function App() {
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <Sidebar />
     </>
   )
 }
